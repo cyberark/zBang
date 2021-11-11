537e2f9c57b8cc1de19c9f3e70356934672d8cf2
yanivyakobovich <yaniv.yakobovich@gmail.com>
2021-11-11 10:57:33 +0200

    Modify the version of zBang exe

--- SourceCode/src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
+++ SourceCode/src/Graphviz4Net.Core/Dot/AntlrParser/Generated/DotGrammarLexer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2021-06-08 18:22:08
+// $ANTLR 3.3.1.7705 ./Dot/AntlrParser/DotGrammar.g 2021-11-11 10:49:16
 
 // The variable 'variable' is assigned but its value is never used.
 #pragma warning disable 219
